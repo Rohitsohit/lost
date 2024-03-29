c49189e4fbeeca94b4fb0e2bd91133f144271793
Rohitsohit <itsrohitemail@gmail.com>
2024-03-29 14:42:46 -0400

    backend step

--- Backend/index.js
+++ Backend/index.js
@@ -4,12 +4,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-
-
 const app = express();
-
-
-
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -18,12 +13,11 @@ app.use(cors());
 
 
 
-// const CONNECTION_URL =
-//   "mongodb+srv://Rohit:123@cluster0.xqoa6nc.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+ const CONNECTION_URL ="mongodb+srv://Rohit:123@cluster0.xqoa6nc.mongodb.net/?retryWrites=true&w=majority";
+const PORT = 5001;
 
 mongoose
-  .connect(process.env.CONNECTION_URL)
+  .connect(CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
   )
