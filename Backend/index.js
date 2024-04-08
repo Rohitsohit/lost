5176b4f1b74678ce55477069d1e7b76a9818780d
Rohitsohit <itsrohitemail@gmail.com>
2024-04-08 10:42:52 -0400

    chat backend

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 import user from "./routes/user.js";
 import item from './routes/item.js'
-
+import ChatRoute from "./routes/chatRoutes.js"
+import MessageRoute from "./routes/messageRoutes.js"
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -13,8 +14,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/user",user);
+
 app.use("/lost-items",item)
 
+app.use('/chat', ChatRoute)
+app.use('/message', MessageRoute)
 
  const CONNECTION_URL ="mongodb+srv://Rohit:123@cluster0.xqoa6nc.mongodb.net/?retryWrites=true&w=majority";
 const PORT = 5001;
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
 
-const messageSchema = mongoose.Schema({
-    username: { type: String},
-    chat: { type: String },
-});
-
 const lostUserSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     id: { type: String },
-    message: [messageSchema],
 });
 
 export default mongoose.model('LostProjectUser', lostUserSchema);
