71ecb258933d214cbddfe32d13aea56ab8b0dc6e
Rohitsohit <itsrohitemail@gmail.com>
2024-04-15 01:00:25 -0400

    reducer Auth error fixed

--- Frontend/src/actions/auth.js
+++ Frontend/src/actions/auth.js
@@ -6,7 +6,7 @@ import { AUTH } from "../constants/actionTypes";
 export const signin = (formData,history) => async (dispatch) => {
   try {
     
-    const data = await api.signIn(formData);
+    const {data} = await api.signIn(formData);
     dispatch({ type: AUTH, data });
     history("/");
   } catch (error) {
@@ -15,7 +15,7 @@ export const signin = (formData,history) => async (dispatch) => {
 };
 export const signup = (formData,history) => async (dispatch) => {
   try {
-    const data = await api.signUp(formData);
+    const {data} = await api.signUp(formData);
    dispatch({ type: AUTH, data });
     history("/");
   } catch (error) {
