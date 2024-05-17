383b74ff51807c96105fcb56aa19bd4b00ac8881
Rohitsohit <itsrohitemail@gmail.com>
2024-05-16 20:33:00 -0400

    Backend Link added

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL:'https://lost-roxn.onrender.com'})
+const API = axios.create({baseURL:'https://lost-backend-api.vercel.app'})
 //const API = axios.create({baseURL:'http://localhost:5001'})
 
 
