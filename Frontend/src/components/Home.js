6b98ce6d17e50fa245d15342c8f1939a567374a8
Rohitsohit <itsrohitemail@gmail.com>
2024-05-21 01:54:06 -0400

    responsvie

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -15,13 +15,6 @@ export default function Home() {
     const dispatch = useDispatch();
 
     const [searchKey, setSearchKey] = useState("");
-    const [showNotifications, setShowNotifications] = useState(false);
-    const [notifications, setNotifications] = useState([
-        { id: 1, message: 'New message 1' },
-        { id: 2, message: 'New message 2' },
-        { id: 3, message: 'New message 3' },
-    ]);
-
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const toggleSidebar = () => {
@@ -29,7 +22,6 @@ export default function Home() {
     };
 
     const handleSearch = (e) => {
-        e.preventDefault();
         setSearchKey(e.target.value.toLowerCase());
     };
 
@@ -53,7 +45,7 @@ export default function Home() {
                 </div>
                 <div className="flex flex-col flex-1 overflow-y-auto">
                     <nav className="flex-1 px-2 py-4 bg-gray-800">
-                        <Link to="/" onClick={(e) => setSearchKey(e.target.value = '')} className="flex items-center rounded-full px-4 py-2 text-gray-100 hover:bg-gray-100 no-underline">
+                        <Link to="/" onClick={() => setSearchKey('')} className="flex items-center rounded-full px-4 py-2 text-gray-100 hover:bg-gray-100 no-underline">
                             Browse All
                         </Link>
                         {isUser ? (
@@ -75,10 +67,10 @@ export default function Home() {
                             Category
                         </h4>
                         <div className="flex justify-center gap-2 flex-wrap p-2">
-                            <button className="bg-gray-100 rounded-full px-2 py-1 text-sm font-semibold text-gray-600" onClick={(e) => setSearchKey(e.target.value = 'card')} >card</button>
-                            <button className="bg-gray-100 rounded-full px-2 py-1 text-sm font-semibold text-gray-600" onClick={(e) => setSearchKey(e.target.value = 'wallet')}>wallet</button>
-                            <button className="bg-gray-100 rounded-full px-2 py-1 text-sm font-semibold text-gray-600" onClick={(e) => setSearchKey(e.target.value = 'Key')}>Keys</button>
-                            <button className="bg-gray-100 rounded-full px-2 py-1 text-sm font-semibold text-gray-600" onClick={(e) => setSearchKey(e.target.value = 'document')}>document</button>
+                            <button className="bg-gray-100 rounded-full px-2 py-1 text-sm font-semibold text-gray-600" onClick={() => setSearchKey('card')} >card</button>
+                            <button className="bg-gray-100 rounded-full px-2 py-1 text-sm font-semibold text-gray-600" onClick={() => setSearchKey('wallet')}>wallet</button>
+                            <button className="bg-gray-100 rounded-full px-2 py-1 text-sm font-semibold text-gray-600" onClick={() => setSearchKey('Key')}>Keys</button>
+                            <button className="bg-gray-100 rounded-full px-2 py-1 text-sm font-semibold text-gray-600" onClick={() => setSearchKey('document')}>document</button>
                         </div>
                     </nav>
                 </div>
@@ -86,30 +78,30 @@ export default function Home() {
 
             {/* Main Content */}
             <div className="flex flex-col flex-1 overflow-y-auto">
-                <div className="flex items-center justify-between h-16 bg-white border-b border-gray-200">
-                    <div className="flex items-center px-4">
-                        <button onClick={toggleSidebar} className="text-gray-500 focus:outline-none hover:text-gray-900">
+                <div className="flex items-center justify-between md:h-16 bg-white border-b border-gray-200 px-4 md:px-0">
+                    <div className="flex items-center">
+                        <button onClick={toggleSidebar} className="text-gray-500 focus:outline-none md:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
                         <input
-                            className="mx-4 w-full border rounded-md px-4 py-2 focus:border-gray-900 hover:text-gray-00"
+                            className="mx-2 md:w-64 border rounded-md px-3 py-2 focus:border-gray-900 hover:text-gray-700"
                             type="text"
                             onChange={handleSearch}
                             placeholder="Search your item here."
                         />
                     </div>
-                    <div className="flex items-center pr-4">
+                    <div className="flex items-center pr-4 space-x-5">
                         {isUser ? (
-                            <div className="flex items-center space-x-5">
+                            <>
                                 <Link to="/messages" className="bg-gray-900 text-white px-4 py-2 rounded hover:bg-gray-700 no-underline">
                                     Messages
                                 </Link>
                                 <Link onClick={handleLogOut} className="bg-gray-900 text-white px-4 py-2 rounded hover:bg-gray-700 no-underline">
                                     LogOut
                                 </Link>
-                            </div>
+                            </>
                         ) : (
                             <Link to="/auth" className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 no-underline">
                                 Sign In
